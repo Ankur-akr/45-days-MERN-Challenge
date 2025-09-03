9fb2b91ebe8a163ef553a8824b8f326e0aa3ce98
Ankur Rai <ankurrai5711@gmail.com>
2025-09-03 22:01:49 +0530

    Add files via upload

---
+++ Day 7/Server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/api', (req, res) => {
+res.json({ message: 'API is running!' });
+});
+
+app.get('/', (req, res) => {
+res.send('Hello from Express! Visit /api for JSON response');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+console.log(`Server running on http://localhost:${PORT}`);
+});
